095441fee6d2ee6fead456dc218fcd0cfb065ff5
mateusamarall <mateus.amaral018@gmail.com>
2020-11-14 20:07:53 -0300

    refeito todo a estrutura para ver se funciona

--- src/index.js
+++ src/index.js
@@ -1,43 +1,79 @@
-var Twitter = require('twit');
+var twit = require('twit');
 require('dotenv').config();
 
 
 
-const Tweet = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000,
+
+
+
+
+const T = new twit({
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
 });
 console.log('rodando....');
 
-function action(event){
-  const {retweeted_status, id_str, screen_name, is_quote_status} = event;
-  const {name} = event.user;
 
-  if(!retweeted_status && !is_quote_status){ // Se o status não for um retweet normal, nem um retweet com comentário
-    Tweet.post(`statuses/retweet/${id_str}`, erro => { 
-      if(erro){
-        console.log("Erro no retweet: " + erro)
-        // Caso haja um erro, informamos no console o mesmo
-      }else {
-        console.log("RETWEETADO: ", `https://twitter.com/${name}/status/${id_str}`)
-        // Se der tudo certo, informamos no console junto com o URL do tweet retweetado
-      }
-    }) // Retweetar o tweet, e caso haja um erro, avisar no console. Se não, avisar no console que deu certo com o id do tweet
-    
+function retweet(searchText) {
+  // Params to be passed to the 'search/tweets' API endpoint
+  let params = {
+      q : searchText ,
+      result_type : 'mixed',
+      count : 25,
   }
-  else {
-       return 
-       // Caso as condições não sejam atendidas, retornar a função vazia, indo para o próximo tweet
-     }
+
+  T.get('search/tweets', params, function(err_search, data_search, response_search){
+
+      let tweets = data_search.statuses
+      if (!err_search)
+      {
+          let tweetIDList = []
+          for(let tweet of tweets) {
+            if(tweet.text.startsWith("RT @")) {
+              if(tweet.retweeted_status) {
+                 tweetIDList.push(tweet.retweeted_status.id_str);
+              }
+              else {
+                 tweetIDList.push(tweet.id_str);
+              }
+             }
+             else {
+                 tweetIDList.push(tweet.id_str);
+             }
+          }
+
+          
+        function onlyUnique(value, index, self) { 
+            return self.indexOf(value) === index;
+        }
+
+        // Get only unique entries
+        tweetIDList = tweetIDList.filter( onlyUnique )
+
+          for (let tweetID of tweetIDList) {
+              T.post('statuses/retweet/:id', {id : tweetID}, function(err_rt, data_rt, response_rt){
+                  if(!err_rt){
+                      console.log("\n\nRetweeted! ID - " + tweetID)
+                  }
+                  else {
+                      console.log("\nError... Duplication maybe... " + tweetID)
+                      console.log("Error = " + err_rt)
+                  }
+              })
+          }
+      }
+      else {
+          console.log("Error while searching" + err_search)
+          process.exit(1)
+      }
+  })
 }
-var stream = Tweet.stream('statuses/filter', {track: '#divulgameutrampo'}) 
-// Aqui dizemos para o programa verificar em modo streaming
-stream.on('data', action) 
-// Ao receber as informações (`data`), passar elas para a função action e chamar a mesma.
-stream.on('error', erro => console.log("Erro: "+ erro)) 
-// Caso haja algum erro, jogar o erro no console para verificarmos.
+
+setInterval(function() { retweet('#divulgameutrampo OR #DivulgaMeuTrampo'); }, 60000)
+
+
+
 
 
